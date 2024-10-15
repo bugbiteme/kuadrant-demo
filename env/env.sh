a8f27bc0b737a2d1ce8de1abcc45aa7e3507e2ef
bugbiteme <leon.s.levy@gmail.com>
2024-10-15 14:50:46 -0700

    gitignore env

--- env/env.sh
+++
@@ -1,3 +0,0 @@
-export AWS_ACCESS_KEY_ID=xxxxxxx # Key ID from AWS with Route 53 access
-export AWS_SECRET_ACCESS_KEY=xxxxxxx # Access key from AWS with Route 53 access
-export REDIS_URL=redis://user:xxxxxx@some-redis.com:10340 # A Redis cluster URL
